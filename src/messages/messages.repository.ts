fbb41d9ab21f7468a5d496640d0e1ca1e6065a90
Felipe Pedrosa <marconizero@gmail.com>
2023-03-14 11:15:43 -0300

    feat: create messages repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,24 @@
+import { readFile, writeFile } from 'fs/promises'
+
+export class MessagesRepository {
+    async findOne<T>(id: string): Promise<T[]> {
+        const contents =  await readFile('messages.json', 'utf8')
+        const messages = JSON.parse(contents)
+        return messages[id]
+    }
+
+    async findAll<T>(): Promise<T[]> {
+        const contents =  await readFile('messages.json', 'utf8')
+        const messages = JSON.parse(contents)
+        return messages
+    }
+
+    async create(content: string): Promise<void> {
+        const contents =  await readFile('messages.json', 'utf8')
+        const messages = JSON.parse(contents)
+        const id = Math.floor(Math.random() * 999)
+        messages[id] = { id, content}
+        await writeFile('messages.json', JSON.stringify(messages))
+        return
+    }
+}
