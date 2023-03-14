5a7a45c9da3e90406ad32b4009e672639fdbc85c
Felipe Pedrosa <marconizero@gmail.com>
2023-03-14 11:40:22 -0300

    refactor: apply messages service on controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,23 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common'
 import { CreateMessageDto } from './dtos/create-message.dto'
+import { MessagesService } from './messages.service'
 
 @Controller('messages')
 export class MessagesController {
+    constructor(private readonly messagesService: MessagesService = new MessagesService()) {}
+
     @Get()
-    listMessages() {
-        console.log('any message')
+    async listMessages<T>(): Promise<T[]> {
+        return await this.messagesService.findAll()
     }
 
     @Post()
-    createMessage(@Body() body: CreateMessageDto) {
-        console.log(body)
+    async createMessage(@Body() body: CreateMessageDto) {
+        await this.messagesService.create(body.content)
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        console.log(id)
+    async getMessage<T>(@Param('id') id: string): Promise<T[]> {
+        return await this.messagesService.findOne(id)
     }
 }
